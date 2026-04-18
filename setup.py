e7fce1741e34b10876df1dc22e69333f2db78456
Keval Savaliya <kevalsavaliya4445@gmail.com>
2026-04-18 16:17:10 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Keval Savaliya",
+    author_email="skeval1601@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
