73bf334504eabbe63edebca9ec4723da1072c55e
Keval Savaliya <kevalsavaliya4445@gmail.com>
2026-04-18 10:46:00 +0200

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# creating Directory
+
+mkdir -p src
+mkdir -p research
+
+# creating files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch setup.py
+touch .env
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.de
+
+echo "Directory and files are created successfully..!"
